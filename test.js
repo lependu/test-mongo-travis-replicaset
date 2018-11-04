e675767d7e893a121990bb58b08f2afad1fa8777
Ákos Kovács <lependu@protonmail.com>
2018-11-04 21:27:35 +0100

    Set up with minimal travis image and container based test

--- test.js
+++ test.js
@@ -3,17 +3,22 @@
 const { test } = require('tap')
 const { MongoClient } = require('mongodb')
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017,127.0.0.1:27018/test?replicaSet=testReplicaSet'
+const MONGO_URI = 'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/test?replicaSet=rs0'
 
 test('Smoke', t => {
-  t.plan(4)
+  t.plan(6)
 
-  MongoClient(MONGO_URI, { useNewUrlParser: true }, function (err, client) {
+  MongoClient.connect(MONGO_URI, { useNewUrlParser: true }, function (err, client) {
     t.error(err)
+    t.tearDown(() => client.close())
+
     t.ok(client)
 
     const db = client.db('test')
+    t.ok(db)
+
     const col = db.collection('testCollection')
+    t.ok(col)
 
     col.insertOne({ a: 1 }, (err, res) => {
       t.error(err)
